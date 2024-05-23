809b642747684852b53b51a7edcfcae7a4fd15ca
dabby1234 <161754750+dabby1234@users.noreply.github.com>
2024-05-23 17:36:02 -0300

    Add files via upload

---
+++ aula6/estado-civil.php
@@ -0,0 +1,27 @@
+<?php
+$estadoCivil = $_GET['estadoCivil'];
+$nome = $_GET['nome'];
+
+
+switch ($estadoCivil) {
+    case '1':
+        $estadoCivil = "Solteiro";
+        break;
+    case '2':
+        $estadoCivil = "Casado";
+        break;
+    case '3':
+        $estadoCivil = "Divorciado";
+        break;
+
+    case '4':
+        $estadoCivil = "Viuvo";
+        break;
+    case '5':
+        $estadoCivil = "União estável";
+        break;
+
+}
+
+echo "O estado civil do " . $nome . "é" . $estadoCivil;
+?>
